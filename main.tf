f01f28dc21616066112ba2fb87c6be35fe79950a
Kabu <t.kaburagi@me.com>
2023-01-12 08:53:20 +0900

    tagged

--- main.tf
+++ main.tf
@@ -9,4 +9,7 @@ resource "aws_instance" "hello-tf-instance" {
   ami = var.ami
   count = var.hello_tf_instance_count
   instance_type = var.hello_tf_instance_type
+  tags = {
+    owner = "me"
+  }
 }
