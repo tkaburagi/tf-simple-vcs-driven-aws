24f4001c0e8d7e4b2c536bfb1ffc7427a7b996a3
tkaburagi <t.kaburagi@me.com>
2023-01-12 08:51:58 +0900

    first commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+		default = "ap-northeast-1"
+}
+variable "ami" {
+		default = "ami-02b658ac34935766f"
+}
+variable "hello_tf_instance_count" {
+    default = 1
+}
+variable "hello_tf_instance_type" {
+    default = "t2.micro"
+}
